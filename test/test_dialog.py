872ea2c91a86c887329bc756e09841982f25fcb4
Benjamin Fuchs <email@benjaminfuchs.de>
2020-04-02 10:03:19 +0200

    Rework test methods.

--- test/test_dialog.py
+++ test/test_dialog.py
@@ -7,18 +7,13 @@ class TestDialog(TestCase):
 
     @staticmethod
     def test_joining_one_player():
-        with Presenter() as presenter:
-            Player("AFFEAFFE")
-
+        with Presenter() as presenter, Player("AFFEAFFE"):
             actual_name = presenter.get_player_name(1)
             assert actual_name == "AFFEAFFE"
 
     @staticmethod
     def test_joining_two_player():
-        with Presenter() as presenter:
-            Player("AFFEAFFE")
-            Player("DEADBEEF")
-
+        with Presenter() as presenter, Player("AFFEAFFE"), Player("DEADBEEF"):
             actual_name = presenter.get_player_name(1)
             assert actual_name == "AFFEAFFE"
             actual_name = presenter.get_player_name(2)
